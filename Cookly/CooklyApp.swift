3d752cab486c47be096fdf991e05ace7fe44a730
TOPAL Mahmut-Ali <mt254663@d9jqpjxwc0.edu.atlas.local>
2025-11-04 16:38:21 +0100

    Initial commit

---
+++ Cookly/CooklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CooklyApp.swift
+//  Cookly
+//
+//  Created by TOPAL Mahmut-Ali on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CooklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

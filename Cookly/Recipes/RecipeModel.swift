687c78ee14861da37bd2fb7eb1cdcd98ddc52d05
TOPAL Mahmut-Ali <mt254663@d9jqpjxwc0.edu.atlas.local>
2025-11-05 10:42:21 +0100

    refactor: Corrige variable ReccipeModel to RecipeModell

--- Cookly/Recipes/RecipeModel.swift
+++ Cookly/Recipes/RecipeModel.swift
@@ -5,13 +5,13 @@
 //  Created by TOPAL Mahmut-Ali on 05/11/2025.
 //
 
-struct ReccipeModel: Codable, Identifiable {
-    let id: String
+struct RecipeModel: Codable, Identifiable {
+    let id: Int
     let title: String
     let image: String
     let time: String
     let difficulty: String
     let category: String
     let ingredients: [String]
-    let steps: [String]    
+    let steps: [String]
 }

7ea0ac63629b3f62848ceedf655093e5037326a2
TOPAL Mahmut-Ali <mt254663@d9jqpjxwc0.edu.atlas.local>
2025-11-05 10:40:40 +0100

    Add RecipesViewModel

---
+++ Cookly/Recipes/RecipesViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  RecipesViewModel.swift
+//  Cookly
+//
+//  Created by TOPAL Mahmut-Ali on 05/11/2025.
+//
+
+import Foundation
+
+@Observable
+class RecipesViewModel {
+    private let repository: RecipesRepository
+    var recipes: [RecipeModel] = []
+    var isLoading: Bool = false
+    var errorMessage: String?
+    
+     init(repository: RecipesRepository = MockRecipesRepository()) {
+        self.repository = repository
+    }
+    
+    func loadingRecipes() async {
+        isLoading = true
+        errorMessage = nil
+        do {
+            recipes = try await repository.fetchRecipes()
+            print("\(recipes.count) recettes chargées")
+        } catch {
+            errorMessage = "Erreur: \(error.localizedDescription)"
+        }
+        isLoading = false
+    }
+    
+}
